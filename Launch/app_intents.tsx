4e203894a57dfe8cadccb80d1788416dfea55ba0
cc166 <85510810+cc166@users.noreply.github.com>
2026-08-15 13:39:55 +0800

    sync: Launch/app_intents.tsx

--- Launch/app_intents.tsx
+++ Launch/app_intents.tsx
@@ -1,9 +1,22 @@
-import { AppIntentManager, AppIntentProtocol, Widget } from 'scripting'
+import { AppIntentManager, AppIntentProtocol, Script, Widget } from 'scripting'
+import { runButtonById } from './buttonCode'
 
 export const OpenAppIntent = AppIntentManager.register({
   name: 'OpenAppIntent',
   protocol: AppIntentProtocol.AppIntent,
   perform: async (bundleId: string) => {
     Widget.openApp(bundleId)
+  }
+})
+
+export const RunButtonIntent = AppIntentManager.register({
+  name: 'RunButtonIntent',
+  protocol: AppIntentProtocol.AppIntent,
+  perform: async (appId: string) => {
+    try {
+      await runButtonById(appId, { env: Script.env })
+    } catch (e) {
+      console.error(e)
+    }
   }
 })
